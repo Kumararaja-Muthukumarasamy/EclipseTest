cb425a2c843590fd49bcdf351cabf7b65f6b0714
kumar <reach.mdkumar@gmail.com>
2023-01-06 12:39:10 +0530

    "Added Another Class"

---
+++ GitHub-Eclipse/src/GitHub/SecondClass.java
@@ -0,0 +1,10 @@
+package GitHub;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		System.out.println("This Project is to integerate with Git Hub from Eclipse");
+
+	}
+
+}
